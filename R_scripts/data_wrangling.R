959f8b79699a985debb13c680cb953c361b5888a
Steven Mamet <sdmamet@gmail.com>
2023-01-20 09:39:48 -0600

    Updated to pull data directly from Mongo. Added calibration constants for sensors in the pseudo lunch room downstairs.

--- R_scripts/data_wrangling.R
+++ R_scripts/data_wrangling.R
@@ -2,11 +2,14 @@ rm(list = ls())
 
 library(tidyverse)
 library(lubridate)
+library(mongolite)
+library(stringr)
 
 # Read in the data ----
-data_input <- "C:/Users/smame/OneDrive/Desktop/EMS_Git/ag_sensors/R_scripts"
-setwd(data_input)
-df <- read.csv("../data/Kernen_20221123_20230102.csv")
+# Read the data in directly from Mongo
+connection_string <- str_sub(read_file("C:/Users/smame/OneDrive - Environmental Material Science Inc/Data Analytics/Admin/mongo_db.txt"), 2, -2)
+sensor_data <- mongo(db="EMS-Database", collection = "Agriculture_Data", url=connection_string)
+df <- sensor_data$find()
 
 # Only keep data post-installation ----
 df <- df %>% 
@@ -72,36 +75,36 @@ calib_coefs <- list(
     co2_k = 0.087704798
   ),
   N03 = data.frame(
-    n2o_alpha = ,
-    n2o_beta = ,
-    n2o_zero = ,
-    n2o_span = ,
-    n2o_a = ,
-    n2o_n = ,
-    n2o_k = ,
-    co2_alpha = ,
-    co2_beta = ,
-    co2_zero = ,
-    co2_span = ,
-    co2_a = ,
-    co2_n = ,
-    co2_k = 
+    n2o_alpha = 0,
+    n2o_beta = 0,
+    n2o_zero = 1.114758832,
+    n2o_span = 0.019818491,
+    n2o_a = 0.018210748,
+    n2o_n = 0.502215922,
+    n2o_k = 1,
+    co2_alpha = 0,
+    co2_beta = 0,
+    co2_zero = 0.739273882,
+    co2_span = 1.271,
+    co2_a = 0.070912706,
+    co2_n = 0.537503111,
+    co2_k = 0.098296
   ),
   N04 = data.frame(
-    n2o_alpha = ,
-    n2o_beta = ,
-    n2o_zero = ,
-    n2o_span = ,
-    n2o_a = ,
-    n2o_n = ,
-    n2o_k = ,
-    co2_alpha = ,
-    co2_beta = ,
-    co2_zero = ,
-    co2_span = ,
-    co2_a = ,
-    co2_n = ,
-    co2_k = 
+    n2o_alpha = 0,
+    n2o_beta = 0,
+    n2o_zero = 1.180376168,
+    n2o_span = 0.01482581,
+    n2o_a = 0.014297708,
+    n2o_n = 0.414519234,
+    n2o_k = 1,
+    co2_alpha = 0,
+    co2_beta = 0,
+    co2_zero = 0.659375302,
+    co2_span = 1.203,
+    co2_a = 0.043928437,
+    co2_n = 0.672816898,
+    co2_k = 0.098
   ),
   # N05 failed calibration
   N05 = data.frame(
@@ -119,22 +122,21 @@ calib_coefs <- list(
     co2_n = NA
   ),
   N06 = data.frame(
-    n2o_alpha = ,
-    n2o_beta = ,
-    n2o_zero = ,
-    n2o_span = ,
-    n2o_a = ,
-    n2o_n = ,
-    n2o_k = ,
-    co2_alpha = ,
-    co2_beta = ,
-    co2_zero = ,
-    co2_span = ,
-    co2_a = ,
-    co2_n = ,
-    co2_k = 
-  ),
-  
+    n2o_alpha = 0,
+    n2o_beta = 0,
+    n2o_zero = 1.153387071,
+    n2o_span = 0.033732728,
+    n2o_a = 0.025355172,
+    n2o_n = 0.415916648,
+    n2o_k = 0,
+    co2_alpha = 0,
+    co2_beta = 0,
+    co2_zero = 0.739273882,
+    co2_span = 1.271,
+    co2_a = 0.21819119,
+    co2_n = 0.260112481,
+    co2_k = 0.06
+  )
 )
 
 # Add constants to df ----
@@ -167,6 +169,7 @@ df <- df %>%
   mutate(co2_a = replace(co2_a, sensor == 1, calib_coefs[[3]]$co2_a)) %>%
   mutate(co2_n = replace(co2_n, sensor == 1, calib_coefs[[3]]$co2_n)) %>%
   mutate(co2_k  = replace(co2_k,  sensor == 1, calib_coefs[[3]]$co2_k)) %>% 
+  
   mutate(n2o_alpha = replace(n2o_alpha, sensor == 2, calib_coefs[[4]]$n2o_alpha)) %>%
   mutate(n2o_beta  = replace(n2o_beta,  sensor == 2, calib_coefs[[4]]$n2o_beta)) %>%
   mutate(n2o_zero = replace(n2o_zero, sensor == 2, calib_coefs[[4]]$n2o_zero)) %>%
@@ -181,20 +184,66 @@ df <- df %>%
   mutate(co2_a = replace(co2_a, sensor == 2, calib_coefs[[4]]$co2_a)) %>%
   mutate(co2_n = replace(co2_n, sensor == 2, calib_coefs[[4]]$co2_n)) %>%
   mutate(co2_k  = replace(co2_k,  sensor == 2, calib_coefs[[4]]$co2_k)) %>% 
-  mutate(n2o_alpha = replace(n2o_alpha, sensor == 5, calib_coefs[[4]]$n2o_alpha)) %>%
-  mutate(n2o_beta  = replace(n2o_beta,  sensor == 5, calib_coefs[[4]]$n2o_beta)) %>%
-  mutate(n2o_zero = replace(n2o_zero, sensor == 5, calib_coefs[[4]]$n2o_zero)) %>%
-  mutate(n2o_span  = replace(n2o_span,  sensor == 5, calib_coefs[[4]]$n2o_span)) %>%
-  mutate(n2o_a = replace(n2o_a, sensor == 5, calib_coefs[[4]]$n2o_a)) %>%
-  mutate(n2o_n = replace(n2o_n, sensor == 5, calib_coefs[[4]]$n2o_n)) %>%
-  mutate(n2o_k = replace(n2o_k, sensor == 5, calib_coefs[[4]]$n2o_k)) %>%
-  mutate(co2_alpha = replace(co2_alpha, sensor == 5, calib_coefs[[4]]$co2_alpha)) %>%
-  mutate(co2_beta = replace(co2_beta, sensor == 5, calib_coefs[[4]]$co2_beta)) %>%
-  mutate(co2_zero = replace(co2_zero, sensor == 5, calib_coefs[[4]]$co2_zero)) %>%
-  mutate(co2_span = replace(co2_span, sensor == 5, calib_coefs[[4]]$co2_span)) %>%
-  mutate(co2_a = replace(co2_a, sensor == 5, calib_coefs[[4]]$co2_a)) %>%
-  mutate(co2_n = replace(co2_n, sensor == 5, calib_coefs[[4]]$co2_n)) %>%
-  mutate(co2_k  = replace(co2_k,  sensor == 5, calib_coefs[[4]]$co2_k))
+  
+  mutate(n2o_alpha = replace(n2o_alpha, sensor == 3, calib_coefs[[5]]$n2o_alpha)) %>%
+  mutate(n2o_beta  = replace(n2o_beta,  sensor == 3, calib_coefs[[5]]$n2o_beta)) %>%
+  mutate(n2o_zero = replace(n2o_zero, sensor == 3, calib_coefs[[5]]$n2o_zero)) %>%
+  mutate(n2o_span  = replace(n2o_span,  sensor == 3, calib_coefs[[5]]$n2o_span)) %>%
+  mutate(n2o_a = replace(n2o_a, sensor == 3, calib_coefs[[5]]$n2o_a)) %>%
+  mutate(n2o_n = replace(n2o_n, sensor == 3, calib_coefs[[5]]$n2o_n)) %>%
+  mutate(n2o_k = replace(n2o_k, sensor == 3, calib_coefs[[5]]$n2o_k)) %>%
+  mutate(co2_alpha = replace(co2_alpha, sensor == 3, calib_coefs[[5]]$co2_alpha)) %>%
+  mutate(co2_beta = replace(co2_beta, sensor == 3, calib_coefs[[5]]$co2_beta)) %>%
+  mutate(co2_zero = replace(co2_zero, sensor == 3, calib_coefs[[5]]$co2_zero)) %>%
+  mutate(co2_span = replace(co2_span, sensor == 3, calib_coefs[[5]]$co2_span)) %>%
+  mutate(co2_a = replace(co2_a, sensor == 3, calib_coefs[[5]]$co2_a)) %>%
+  mutate(co2_n = replace(co2_n, sensor == 3, calib_coefs[[5]]$co2_n)) %>%
+  mutate(co2_k  = replace(co2_k,  sensor == 3, calib_coefs[[5]]$co2_k)) %>% 
+  
+  mutate(n2o_alpha = replace(n2o_alpha, sensor == 4, calib_coefs[[6]]$n2o_alpha)) %>%
+  mutate(n2o_beta  = replace(n2o_beta,  sensor == 4, calib_coefs[[6]]$n2o_beta)) %>%
+  mutate(n2o_zero = replace(n2o_zero, sensor == 4, calib_coefs[[6]]$n2o_zero)) %>%
+  mutate(n2o_span  = replace(n2o_span,  sensor == 4, calib_coefs[[6]]$n2o_span)) %>%
+  mutate(n2o_a = replace(n2o_a, sensor == 4, calib_coefs[[6]]$n2o_a)) %>%
+  mutate(n2o_n = replace(n2o_n, sensor == 4, calib_coefs[[6]]$n2o_n)) %>%
+  mutate(n2o_k = replace(n2o_k, sensor == 4, calib_coefs[[6]]$n2o_k)) %>%
+  mutate(co2_alpha = replace(co2_alpha, sensor == 4, calib_coefs[[6]]$co2_alpha)) %>%
+  mutate(co2_beta = replace(co2_beta, sensor == 4, calib_coefs[[6]]$co2_beta)) %>%
+  mutate(co2_zero = replace(co2_zero, sensor == 4, calib_coefs[[6]]$co2_zero)) %>%
+  mutate(co2_span = replace(co2_span, sensor == 4, calib_coefs[[6]]$co2_span)) %>%
+  mutate(co2_a = replace(co2_a, sensor == 4, calib_coefs[[6]]$co2_a)) %>%
+  mutate(co2_n = replace(co2_n, sensor == 4, calib_coefs[[6]]$co2_n)) %>%
+  mutate(co2_k  = replace(co2_k,  sensor == 4, calib_coefs[[6]]$co2_k)) %>% 
+  # Calibration failed for N05, so using N04 values
+  mutate(n2o_alpha = replace(n2o_alpha, sensor == 5, calib_coefs[[6]]$n2o_alpha)) %>%
+  mutate(n2o_beta  = replace(n2o_beta,  sensor == 5, calib_coefs[[6]]$n2o_beta)) %>%
+  mutate(n2o_zero = replace(n2o_zero, sensor == 5, calib_coefs[[6]]$n2o_zero)) %>%
+  mutate(n2o_span  = replace(n2o_span,  sensor == 5, calib_coefs[[6]]$n2o_span)) %>%
+  mutate(n2o_a = replace(n2o_a, sensor == 5, calib_coefs[[6]]$n2o_a)) %>%
+  mutate(n2o_n = replace(n2o_n, sensor == 5, calib_coefs[[6]]$n2o_n)) %>%
+  mutate(n2o_k = replace(n2o_k, sensor == 5, calib_coefs[[6]]$n2o_k)) %>%
+  mutate(co2_alpha = replace(co2_alpha, sensor == 5, calib_coefs[[6]]$co2_alpha)) %>%
+  mutate(co2_beta = replace(co2_beta, sensor == 5, calib_coefs[[6]]$co2_beta)) %>%
+  mutate(co2_zero = replace(co2_zero, sensor == 5, calib_coefs[[6]]$co2_zero)) %>%
+  mutate(co2_span = replace(co2_span, sensor == 5, calib_coefs[[6]]$co2_span)) %>%
+  mutate(co2_a = replace(co2_a, sensor == 5, calib_coefs[[6]]$co2_a)) %>%
+  mutate(co2_n = replace(co2_n, sensor == 5, calib_coefs[[6]]$co2_n)) %>%
+  mutate(co2_k  = replace(co2_k,  sensor == 5, calib_coefs[[6]]$co2_k)) %>% 
+  
+  mutate(n2o_alpha = replace(n2o_alpha, sensor == 6, calib_coefs[[8]]$n2o_alpha)) %>%
+  mutate(n2o_beta  = replace(n2o_beta,  sensor == 6, calib_coefs[[8]]$n2o_beta)) %>%
+  mutate(n2o_zero = replace(n2o_zero, sensor == 6, calib_coefs[[8]]$n2o_zero)) %>%
+  mutate(n2o_span  = replace(n2o_span,  sensor == 6, calib_coefs[[8]]$n2o_span)) %>%
+  mutate(n2o_a = replace(n2o_a, sensor == 6, calib_coefs[[8]]$n2o_a)) %>%
+  mutate(n2o_n = replace(n2o_n, sensor == 6, calib_coefs[[8]]$n2o_n)) %>%
+  mutate(n2o_k = replace(n2o_k, sensor == 6, calib_coefs[[8]]$n2o_k)) %>%
+  mutate(co2_alpha = replace(co2_alpha, sensor == 6, calib_coefs[[8]]$co2_alpha)) %>%
+  mutate(co2_beta = replace(co2_beta, sensor == 6, calib_coefs[[8]]$co2_beta)) %>%
+  mutate(co2_zero = replace(co2_zero, sensor == 6, calib_coefs[[8]]$co2_zero)) %>%
+  mutate(co2_span = replace(co2_span, sensor == 6, calib_coefs[[8]]$co2_span)) %>%
+  mutate(co2_a = replace(co2_a, sensor == 6, calib_coefs[[8]]$co2_a)) %>%
+  mutate(co2_n = replace(co2_n, sensor == 6, calib_coefs[[8]]$co2_n)) %>%
+  mutate(co2_k  = replace(co2_k,  sensor == 6, calib_coefs[[8]]$co2_k))
 
 df_tmp <- 
   data.frame(irSignal1 = 337063.7625,
@@ -232,7 +281,8 @@ df_tmp %>%
          conc_co2_corr = ifelse(co2_norm_absorb_int <= 0, 0, ((-log(1-((1-exp(-co2_norm_absorb_int))/co2_beta_comp)))/co2_a)^(1/co2_n)))
 
 df <- df %>% 
-  mutate(conc_n2o = (-log(1-(1-(irSignal4/(irSignal1*n2o_zero)))/n2o_span)/n2o_a)^(1/n2o_n),
+  mutate(sensor = factor(sensor),
+         conc_n2o = (-log(1-(1-(irSignal4/(irSignal1*n2o_zero)))/n2o_span)/n2o_a)^(1/n2o_n),
          n2o_alpha_comp = (irSignal4/(irSignal1*n2o_zero))*(1 + n2o_alpha*(irtemperature - calib_coefs[[2]])),
          n2o_beta_comp = n2o_span + (n2o_beta*((irtemperature - calib_coefs[[2]])/(calib_coefs[[2]] + calib_coefs[[1]]))),
          conc_co2 = (-log(1-(1-(irSignal3/(irSignal1*co2_zero)))/co2_span)/co2_a)^(1/co2_n),
@@ -247,46 +297,51 @@ df <- df %>%
 
 # Visual inspections ----
 df %>% 
-  ggplot(aes(x = datetime, y = conc_n2o, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = conc_n2o, color = sensor)) + geom_line() + geom_point() + ylim(0,6000)
 
 df %>% 
-  ggplot(aes(x = datetime, y = conc_n2o_corr, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = conc_n2o_corr, color = sensor)) + geom_line() + geom_point() + ylim(0,9000)
 
 df %>% 
   filter(!is.na(conc_co2)) %>%
   ggplot(aes(x = datetime, y = conc_co2, color = sensor)) + geom_line() + geom_point()
 
 df %>% 
-  ggplot(aes(x = datetime, y = conc_co2_corr, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = conc_co2_corr, color = sensor)) + geom_line() + geom_point() + ylim(0,0.5)
 
 df %>% 
   ggplot(aes(x = datetime, y = humidity, color = sensor)) + geom_line() + geom_point()
 
 df %>%
   filter(irSignal1 != 0) %>% 
-  ggplot(aes(x = datetime, y = irSignal1, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = irSignal1, color = sensor)) + geom_line() + geom_point() + ylim(0,500000)
 
 df %>% 
   filter(irSignal2 != 0) %>% 
-  ggplot(aes(x = datetime, y = irSignal2, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = irSignal2, color = sensor)) + geom_line() + geom_point() + ylim(0,500000)
 
 df %>% 
   filter(irSignal3 != 0) %>% 
-  ggplot(aes(x = datetime, y = irSignal3, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = irSignal3, color = sensor)) + geom_line() + geom_point() + ylim(0,500000)
 
 df %>% 
   filter(irSignal4 != 0) %>% 
-  ggplot(aes(x = datetime, y = irSignal4, color = sensor)) + geom_line() + geom_point()
+  ggplot(aes(x = datetime, y = irSignal4, color = sensor)) + geom_line() + geom_point() + ylim(0,500000)
 
 df %>%
   filter(irtemperature < 50) %>% 
   ggplot(aes(x = datetime, y = irtemperature, color = sensor)) + geom_line() + geom_point()
 
 df %>%
-  # filter(irtemperature < 100) %>% 
+  filter(soilTemperature < 10) %>%
   ggplot(aes(x = datetime, y = soilTemperature, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(temperature != 0) %>%
+  ggplot(aes(x = datetime, y = temperature, color = sensor)) + geom_line() + geom_point()
+
+df %>%
+  filter(pressure != 0) %>% 
   ggplot(aes(x = datetime, y = pressure, color = sensor)) + geom_line() + geom_point()
 
 df %>%
@@ -296,23 +351,23 @@ df %>%
   ggplot(aes(x = datetime, y = snr, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(soilConductivity != 0) %>% 
   ggplot(aes(x = datetime, y = soilConductivity, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(soilDielectric != 0) %>% 
   ggplot(aes(x = datetime, y = soilDielectric, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(soilDissolved != 0) %>% 
   ggplot(aes(x = datetime, y = soilDissolved, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(soilMoisture != 0) %>% 
   ggplot(aes(x = datetime, y = soilMoisture, color = sensor)) + geom_line() + geom_point()
 
 df %>%
+  filter(soilSalinity != 0) %>% 
   ggplot(aes(x = datetime, y = soilSalinity, color = sensor)) + geom_line() + geom_point()
 
-df %>%
-  ggplot(aes(x = datetime, y = soilTemperature, color = sensor)) + geom_line() + geom_point()
-
-df %>%
-  ggplot(aes(x = datetime, y = temperature, color = sensor)) + geom_line() + geom_point()
 
